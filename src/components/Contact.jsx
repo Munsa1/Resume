2514e64420cbdd9f54748980162efb52fafec46e
Munsa1 <munsamibenge65@gmail.com>
2024-09-27 12:31:34 +0200

    Deploy app

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -81,18 +81,18 @@ const Contact = () => {
           className='mt-12 flex flex-col gap-8'
         >
           <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your Name</span>
+            <span className='text-white font-medium mb-4'>Munsa Mibenge</span>
             <input
               type='text'
               name='name'
               value={form.name}
               onChange={handleChange}
-              placeholder="What's your good name?"
+              placeholder="What's your name?"
               className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
             />
           </label>
           <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your email</span>
+            <span className='text-white font-medium mb-4'>munsamibenge65@gmail.com</span>
             <input
               type='email'
               name='email'
@@ -103,7 +103,7 @@ const Contact = () => {
             />
           </label>
           <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your Message</span>
+            <span className='text-white font-medium mb-4'>Contact me</span>
             <textarea
               rows={7}
               name='message'
